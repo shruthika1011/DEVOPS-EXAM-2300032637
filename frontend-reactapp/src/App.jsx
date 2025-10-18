7930ab3ad1c9fdbbe04e9cfa5fa1c27ac1718496
Shruthika <shruthikapadma2004@gmail.com>
2025-10-18 11:40:24 +0530

    navbar

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <NavBar />
+      <Navbar />
       </BrowserRouter>
     </div>
   );
